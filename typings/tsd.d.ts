b99c5a0e166cf2486cd95059c11d54c0614562b3
Kitson Kelly <me@kitsonkelly.com>
2016-04-26 13:07:46 +0100

    Update dependencies

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,7 +1,7 @@
 /// <reference path="node/node.d.ts" />
-/// <reference path="../node_modules/dojo-core/typings/dojo-core/dojo-core-2.0.0-alpha.4.d.ts" />
+/// <reference path="../node_modules/dojo-core/typings/dojo-core/dojo-core.d.ts" />
 /// <reference path="../node_modules/dojo-core/typings/symbol-shim/symbol-shim.d.ts" />
-/// <reference path="../node_modules/dojo-loader/typings/dojo-loader/dojo-loader-2.0.0-beta.2.d.ts" />
+/// <reference path="../node_modules/dojo-loader/typings/dojo-loader/dojo-loader.d.ts" />
 /// <reference path="../node_modules/@reactivex/rxjs/typings/main/ambient/es6-shim/index.d.ts" />
 
 declare module 'rxjs/Rx' {
