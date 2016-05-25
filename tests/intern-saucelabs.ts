c5b9fcca2342eeed3b3bec039b9fd7b175897dbd
Kitson Kelly <me@kitsonkelly.com>
2016-05-25 10:48:29 +0100

    Temporarily remove Firefox from CI

--- tests/intern-saucelabs.ts
+++ tests/intern-saucelabs.ts
@@ -4,7 +4,7 @@ export const environments = [
 	{ browserName: 'internet explorer', version: [ '10.0', '11.0' ], platform: 'Windows 7' },
 	/* Intern 3.0.6 sometimes works on Edge */
 	// { browserName: 'microsoftedge', platform: 'Windows 10' },
-	{ browserName: 'firefox', platform: 'Windows 10' },
+	// { browserName: 'firefox', platform: 'Windows 10' },
 	{ browserName: 'chrome', platform: 'Windows 10' },
 	/* Still appear to be issues on Safari on SauceLabs */
 	// { browserName: 'safari', version: '9', platform: 'OS X 10.11' },
