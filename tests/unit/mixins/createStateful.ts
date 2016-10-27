603d8a912c637a45524ffdb0b2a0d5538bd3e46c
Anthony Gubler <agubler@sitepen.com>
2016-10-27 13:30:40 +0100

    revert conversion of mixins to dojo-interfaces

--- tests/unit/mixins/createStateful.ts
+++ tests/unit/mixins/createStateful.ts
@@ -2,9 +2,8 @@ import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
 import { hasToStringTag } from '../../support/util';
 import Promise from 'dojo-shim/Promise';
-import { State } from 'dojo-interfaces/bases';
 import { Observable, Observer } from 'rxjs/Rx';
-import createStateful from '../../../src/mixins/createStateful';
+import createStateful, { State } from '../../../src/mixins/createStateful';
 
 registerSuite({
 	name: 'mixins/createStateful',
