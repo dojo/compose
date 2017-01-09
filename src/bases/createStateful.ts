fa42651bc15f40ccf30e8d61711e309e5df26249
Anthony Gubler <agubler@gmail.com>
2017-01-09 15:44:54 +0000

    simplify stateful and remove observable api (#113)
    * simplify stateful and remove observable api
    
    * move state changed event type out of compose class
    
    * stateful mixin and typo
    
    * add test for state changed event

--- src/bases/createStateful.ts
+++ src/bases/createStateful.ts
@@ -1,84 +1,28 @@
 import { deepAssign } from 'dojo-core/lang';
-import { Handle } from 'dojo-interfaces/core';
 import {
-	Evented,
+	State,
 	Stateful,
-	StatefulOptions,
-	StatefulMixin
+	StatefulMixin,
+	StatefulOptions
 } from 'dojo-interfaces/bases';
-import { StoreObservablePatchable } from 'dojo-interfaces/abilities';
-import { Subscription } from 'dojo-shim/Observable';
 import WeakMap from 'dojo-shim/WeakMap';
 import createEvented from './createEvented';
 import { ComposeFactory } from '../compose';
-import createCancelableEvent from './createCancelableEvent';
-
-export interface StatefulFactory extends ComposeFactory<Stateful<Object>, StatefulOptions<Object>> {
-	<S extends Object>(options?: StatefulOptions<S>): Stateful<S>;
-}
 
 /**
- * Internal interface which contains references to an observed state
+ * Stateful Factory
  */
-interface ObservedState {
-	id: string;
-	observable: StoreObservablePatchable<Object>;
-	subscription: Subscription;
-	handle: Handle;
-}
+export interface StatefulFactory extends ComposeFactory<StatefulMixin<State>, StatefulOptions<State>> {}
 
 /**
- * A weak map of stateful instances to their obseved state references
+ * Private map of internal instance state.
  */
-const observedStateMap = new WeakMap<Stateful<Object>, ObservedState>();
+const instanceStateMap = new WeakMap<Stateful<State>, State>();
 
 /**
- * Internal function to unobserve the state of a `Stateful`.  It emits a `statecomplete` event which can be
- * cancelled.
- *
- * @param stateful The `Stateful` object to unobserve
+ * State change event type
  */
-function completeStatefulState(stateful: Stateful<Object>): void {
-	const observedState = observedStateMap.get(stateful);
-	if (observedState) {
-		observedState.handle.destroy();
-		const statecomplete = createCancelableEvent({
-			type: 'state:completed',
-			target: stateful
-		});
-		stateful.emit(statecomplete);
-		if (!statecomplete.defaultPrevented) {
-			stateful.destroy();
-		}
-	}
-}
-
-/**
- * Internal function that actually applies the state to the Stateful's state and
- * emits the `statechange` event.
- *
- * @param stateful The Stateful instance
- * @param state The State to be set
- */
-function setStatefulState(target: Stateful<Object>, state: Object): void {
-	const previousState = stateWeakMap.get(target);
-	if (!previousState) {
-		throw new Error('Unable to set destroyed state');
-	}
-	const type = 'state:changed';
-	state = deepAssign(previousState, state);
-	const eventObject = {
-		type,
-		state,
-		target
-	};
-	target.emit(eventObject);
-}
-
-/**
- * A weak map that contains the stateful's state
- */
-const stateWeakMap = new WeakMap<Stateful<Object>, Object>();
+const stateChangedEventType = 'state:changed';
 
 /**
  * Create an instance of a stateful object
@@ -87,79 +31,23 @@ const createStateful: StatefulFactory = createEvented
 	.mixin({
 		className: 'Stateful',
 		mixin: {
-			get stateFrom(this: Stateful<Object>): StoreObservablePatchable<Object> | undefined {
-				const observedState = observedStateMap.get(this);
-				if (observedState) {
-					return observedState.observable;
-				}
+			get state(this: Stateful<State>) {
+				return instanceStateMap.get(this);
 			},
-
-			get state(this: Stateful<Object>): Object {
-				return stateWeakMap.get(this);
-			},
-
-			setState(this: Stateful<Object>, value: Object) {
-				const observedState = observedStateMap.get(this);
-				if (observedState) {
-					observedState.observable.patch(value, { id: observedState.id });
-				}
-				else {
-					setStatefulState(this, value);
-				}
-			},
-
-			observeState(this: Stateful<Object>, id: string, observable: StoreObservablePatchable<Object>): Handle {
-				let observedState = observedStateMap.get(this);
-				if (observedState) {
-					if (observedState.id === id && observedState.observable === observable) {
-						return observedState.handle;
-					}
-					throw new Error(`Already observing state with ID '${observedState.id}'`);
-				}
-				const stateful = this;
-				const handle = {
-					destroy() {
-						const observedState = observedStateMap.get(stateful);
-						if (observedState) {
-							observedState.subscription.unsubscribe();
-							observedStateMap.delete(stateful);
-						}
-					}
+			setState<S extends State>(this: Stateful<S>, value: Partial<S>) {
+				const oldState = instanceStateMap.get(this);
+				const state = deepAssign({}, oldState, value);
+				const eventObject = {
+					type: stateChangedEventType,
+					state,
+					target: this
 				};
-				const subscription = observable
-					.observe(id)
-					.subscribe(
-						(state) => {
-							setStatefulState(stateful, state);
-						},
-						(err) => {
-							throw err;
-						},
-						() => {
-							completeStatefulState(stateful);
-						}
-					);
-
-				observedStateMap.set(stateful, { id, observable, subscription: <Subscription> subscription, handle });
-				return handle;
+				instanceStateMap.set(this, state);
+				this.emit(eventObject);
 			}
 		},
-		initialize(instance: StatefulMixin<Object> & Evented, options: StatefulOptions<Object>) {
-			stateWeakMap.set(instance, Object.create(null));
-			instance.own({
-				destroy() {
-					stateWeakMap.delete(instance);
-				}
-			});
-			if (options) {
-				const { id, stateFrom } = options;
-				if (typeof id !== 'undefined' && stateFrom) {
-					instance.own(instance.observeState(id, stateFrom));
-				}
-				else if (stateFrom) {
-					throw new TypeError('When "stateFrom" option is supplied, factory also requires "id" option.');
-				}
-			}
+		initialize(instance: Stateful<State>) {
+			instanceStateMap.set(instance, Object.create(null));
 		}
 	});
 
--- tests/unit/bases/createStateful.ts
+++ tests/unit/bases/createStateful.ts
@@ -1,421 +1,51 @@
 import * as registerSuite from 'intern!object';
 import * as assert from 'intern/chai!assert';
-import { hasToStringTag } from '../../support/util';
-import Promise from 'dojo-shim/Promise';
-import Observable, { SubscriptionObserver } from 'dojo-shim/Observable';
 import createStateful from '../../../src/bases/createStateful';
+import { Stateful, State } from 'dojo-interfaces/bases';
 
 registerSuite({
 	name: 'mixins/createStateful',
-	creation: {
-		'no options'() {
-			const stateful = createStateful();
-			assert.isUndefined(stateful.stateFrom);
-			assert.deepEqual(stateful.state, {}, 'stateful should have empty state');
-		},
-		'with id and stateFrom'() {
-			let called = 0;
-			const observer = {
-				observe(id: string): Observable<Object> {
-					called++;
-					return new Observable(function subscribe(observer: SubscriptionObserver<Object>) {
-						observer.next({ foo: 'bar' });
-						observer.complete();
-					});
-				},
-				patch(value: any, options?: { id?: string }): Promise<Object> {
-					assert.strictEqual(options && options.id, 'foo');
-					return Promise.resolve(value);
-				}
-			};
-
-			const stateful = createStateful<{ foo?: string; }>({
-				id: 'foo',
-				stateFrom: observer
-			});
-
-			assert.strictEqual(called, 1);
-			assert.equal(stateful.stateFrom, observer);
-			assert.deepEqual(stateful.state, { foo: 'bar' });
-		},
-		'with id of 0'() {
-			/* while the interface specifies a string for an ID, real world usage may very well pass
-			 * a numeric ID which will eventually get coerced into a string, therefore the number of
-			 * 0 should be halnded gracefully */
-			let called = 0;
-			const observer = {
-				observe(id: string): Observable<Object> {
-					called++;
-					return new Observable(function subscribe(observer: SubscriptionObserver<Object>) {
-						observer.next({ foo: 'bar' });
-						observer.complete();
-					});
-				},
-				patch(value: any, options?: { id?: string }): Promise<Object> {
-					assert.strictEqual(options && options.id, 0);
-					return Promise.resolve(value);
-				}
-			};
-
-			const stateful = createStateful<{ foo?: string; }>({
-				id: <any> 0,
-				stateFrom: observer
-			});
-
-			assert.strictEqual(called, 1);
-			assert.deepEqual(stateful.state, { foo: 'bar' });
-		},
-		'with only stateForm throws'() {
-			const observer = {
-				observe(id: string): Observable<Object> {
-					return new Observable(() => {});
-				},
-				patch(value: any, options?: { id?: string }): Promise<Object> {
-					return Promise.resolve(value);
-				}
-			};
-
-			assert.throws(() => {
-				createStateful({
-					stateFrom: observer
-				});
-			}, TypeError);
-		}
-	},
-	'destroy()'() {
-		const observer = {
-			observe(id: string): Observable<Object> {
-				return new Observable(function subscribe(observer: SubscriptionObserver<Object>) {
-					observer.next({ foo: 'bar' });
-					observer.complete();
-				});
-			},
-			patch(value: any, options?: { id?: string }): Promise<Object> {
-				throw Error('Should not have been called!');
-			}
-		};
-
-		const stateful = createStateful<{ foo?: string; }>({
-			id: 'foo',
-			stateFrom: observer
-		});
-
-		stateful.destroy();
-
-		assert.isUndefined(stateful.state);
-
-		assert.throws(() => {
-			stateful.setState({ foo: 'bar' });
-		}, Error);
-	},
-	'setState'() {
+	creation() {
 		const stateful = createStateful();
-		stateful.setState({
-			bar: 'foo'
-		});
-
-		assert.deepEqual(stateful.state, { bar: 'foo' });
-		stateful.setState({
-			foo: 1
-		});
-		assert.deepEqual(stateful.state, { foo: 1, bar: 'foo' });
+		assert.deepEqual(stateful.state, {}, 'stateful should have empty state');
+	},
+	'get and set state'() {
+		const stateful = createStateful();
 		const state = {
-			foo: [ { foo: 'bar' }, { foo: 'baz' } ]
+			foo: 'bar'
 		};
 		stateful.setState(state);
-		assert.notStrictEqual((<any> stateful.state).foo, state.foo, 'state should not be strict equal');
-		assert.deepEqual((<any> stateful.state).foo, state.foo, 'state should be deeply equal');
-		stateful.setState({ bar: undefined });
-		assert.isUndefined((<any> stateful.state).bar, 'bar is undefined');
-		state.foo.push({ foo: 'qat' });
-		assert.strictEqual((<any> stateful.state).foo.length, 2, 'state should remain untouched');
+
+		assert.deepEqual(stateful.state, state);
 	},
-	'observe state': {
-		'observeState()'() {
-			let called = 0;
-			let patchCalled = 0;
-			let observerRef: SubscriptionObserver<Object>;
-			const observer = {
-				observe(id: string): Observable<Object> {
-					assert.strictEqual(id, 'foo');
-					called++;
-					return new Observable(function subscribe(observer: SubscriptionObserver<Object>) {
-						observerRef = observer;
-						observerRef.next({ foo: 'bar' });
-					});
-				},
-				patch(value: any, options?: { id?: string }): Promise<Object> {
-					patchCalled++;
-					observerRef.next(value);
-					assert.strictEqual(options && options.id, 'foo');
-					return Promise.resolve(value);
-				}
-			};
+	'partially update state'() {
+		const stateful = createStateful();
+		const state = {
+			foo: 'bar'
+		};
+		const updatedState = {
+			baz: 'qux'
+		};
 
-			const stateful = createStateful();
-
-			stateful.observeState('foo', observer);
-			assert.strictEqual(called, 1);
-			assert.strictEqual(patchCalled, 0);
-			assert.deepEqual(stateful.state, { foo: 'bar' });
-
-			observer.patch({ foo: 'qat' }, { id: 'foo' });
-			assert.strictEqual(called, 1);
-			assert.strictEqual(patchCalled, 1);
-			assert.deepEqual(stateful.state, { foo: 'qat' });
-
-			stateful.setState({ foo: 'foo'});
-			assert.strictEqual(called, 1);
-			assert.strictEqual(patchCalled, 2);
-			assert.deepEqual(stateful.state, { foo: 'foo' });
-		},
-		'observeState() - completed/destroyed'() {
-			let called = 0;
-			let destroyed = 0;
-			let observerRef: SubscriptionObserver<Object> = <any> undefined;
-			const observer = {
-				observe(id: string): Observable<Object> {
-					assert.strictEqual(id, 'foo');
-					return new Observable(function subscribe(observer: SubscriptionObserver<Object>) {
-						observerRef = observer;
-						observerRef.next({ foo: 'bar' });
-					});
-				},
-				patch(value: any, options?: { id?: string }): Promise<Object> {
-					throw new Error('Should not have been called');
-				}
-			};
-
-			const stateful = createStateful();
-
-			stateful.own({
-				destroy() {
-					destroyed++;
-				}
-			});
-
-			stateful.on('state:completed', (evt) => {
-				called++;
-				assert.strictEqual(evt.target, stateful);
-			});
-
-			stateful.observeState('foo', observer);
-			assert.deepEqual(stateful.state, { foo: 'bar' });
-
-			assert.strictEqual(called, 0);
-			assert.strictEqual(destroyed, 0);
-
-			observerRef.complete();
-
-			assert.strictEqual(called, 1);
-			assert.strictEqual(destroyed, 1);
-
-			assert.throws(() => {
-				stateful.setState({ foo: 'qat' });
-			});
-		},
-		'observeState() - completed but preventDefaut'() {
-			let called = 0;
-			let destroyed = 0;
-			let observerRef: SubscriptionObserver<Object> = <any> undefined;
-			const observer = {
-				observe(id: string): Observable<Object> {
-					assert.strictEqual(id, 'foo');
-					return new Observable(function subscribe(observer: SubscriptionObserver<Object>) {
-						observerRef = observer;
-						observerRef.next({ foo: 'bar' });
-					});
-				},
-				patch(value: any, options?: { id?: string }): Promise<Object> {
-					throw new Error('Should not have been called');
-				}
-			};
-
-			const stateful = createStateful();
-
-			stateful.own({
-				destroy() {
-					destroyed++;
-				}
-			});
-
-			stateful.on('state:completed', (evt) => {
-				called++;
-				assert.strictEqual(evt.target, stateful);
-				evt.preventDefault();
-			});
-
-			stateful.observeState('foo', observer);
-			assert.deepEqual(stateful.state, { foo: 'bar' });
-
-			assert.strictEqual(called, 0);
-			assert.strictEqual(destroyed, 0);
-
-			observerRef.complete();
-
-			assert.strictEqual(called, 1);
-			assert.strictEqual(destroyed, 0);
-
-			stateful.setState({ foo: 'qat' });
-			assert.deepEqual(stateful.state, { foo: 'qat' });
-			assert.strictEqual(called, 1);
-			assert.strictEqual(destroyed, 0);
-		},
-		'observeState() - error'() {
-			const observer = {
-				observe(id: string): Observable<Object> {
-					return new Observable(function subscribe(observer: SubscriptionObserver<Object>) {
-						observer.error(new Error('Ooops...'));
-					});
-				},
-				patch(value: any, options?: { id?: string }): Promise<Object> {
-					return Promise.resolve(value);
-				}
-			};
-
-			const stateful = createStateful();
-
-			assert.throws(() => {
-				stateful.observeState('foo', observer);
-			}, Error, 'Ooops...');
-		},
-		'observeState() - again'() {
-			const observer1 = {
-				observe(id: string): Observable<Object> {
-					return new Observable(() => {});
-				},
-				patch(value: any, options?: { id?: string }): Promise<Object> {
-					return Promise.resolve(value);
-				}
-			};
-
-			const observer2 = {
-				observe(id: string): Observable<Object> {
-					return new Observable(() => {});
-				},
-				patch(value: any, options?: { id?: string }): Promise<Object> {
-					return Promise.resolve(value);
-				}
-			};
-
-			const stateful = createStateful({
-				id: 'foo',
-				stateFrom: observer1
-			});
-
-			const handle = stateful.observeState('foo', observer1);
-
-			assert(handle);
-
-			assert.strictEqual(stateful.observeState('foo', observer1), handle, 'same handle should be returned');
-
-			assert.throws(() => {
-				stateful.observeState('bar', observer1);
-			}, Error);
-
-			assert.throws(() => {
-				stateful.observeState('foo', observer2);
-			}, Error);
-		},
-		'observeState() - destroy handle'() {
-			let observerRef: SubscriptionObserver<Object>;
-			const observer = {
-				observe(id: string): Observable<Object> {
-					return new Observable(function subscribe(observer: SubscriptionObserver<Object>) {
-						observerRef = observer;
-						observerRef.next({ foo: 'bar' });
-					});
-				},
-				patch(value: any, options?: { id?: string }): Promise<Object> {
-					observerRef.next(value);
-					return Promise.resolve(value);
-				}
-			};
-
-			const stateful = createStateful();
-
-			const handle = stateful.observeState('foo', observer);
-			assert.deepEqual(stateful.state, { foo: 'bar' });
-
-			handle.destroy();
-			observer.patch({ foo: 'qat' }, { id: 'foo' });
-			assert.deepEqual(stateful.state, { foo: 'bar' });
-
-			assert.doesNotThrow(() => {
-				handle.destroy();
-			});
-		}
+		stateful.setState(state);
+		assert.deepEqual(stateful.state, state);
+		stateful.setState(updatedState);
+		assert.deepEqual(stateful.state, { foo: 'bar', baz: 'qux' });
 	},
-	'"state:changed" event type': {
-		'local state'() {
-			let count = 0;
-			interface TestState {
-				foo?: string;
-			}
+	'emits `state:changed` event on state update'() {
+		const stateful: Stateful<State> = createStateful();
+		const state = {
+			foo: 'bar'
+		};
+		let called = false;
 
-			const stateful = createStateful<TestState>({});
-
-			stateful.on('state:changed', (event) => {
-				count++;
-				assert.strictEqual(event.target, stateful);
-				assert.strictEqual(event.type, 'state:changed');
-				assert.deepEqual(event.state, { foo: 'bar' });
-				assert.strictEqual(event.state, event.target.state);
-			});
-
-			assert.strictEqual(count, 0, 'listener not called yet');
-
-			stateful.setState({ foo: 'bar' });
-
-			assert.strictEqual(count, 1, 'listener called once');
-		},
-		'observed state'() {
-			let count = 0;
-			let patchCount = 0;
-
-			interface TestState {
-				foo?: string;
-			}
-
-			let observerRef: SubscriptionObserver<TestState>;
-
-			const observer = {
-				observe(id: string): Observable<TestState> {
-					return new Observable(function subscribe(observer: SubscriptionObserver<TestState>) {
-						observerRef = observer;
-						observerRef.next({ foo: 'bar' });
-					});
-				},
-				patch(value: any, options?: { id?: string }): Promise<TestState> {
-					patchCount++;
-					observerRef.next(value);
-					return Promise.resolve(value);
-				}
-			};
-
-			const stateful = createStateful({
-				id: 'foo',
-				stateFrom: observer,
-				listeners: { 'state:changed'() { count++; } }
-			});
-
-			assert.deepEqual(stateful.state, { foo: 'bar' });
-
-			assert.strictEqual(count, 1, 'listener not called');
-
-			const state = { foo: 'qat' };
-			stateful.setState(state);
-
-			assert.strictEqual(patchCount, 1, 'patch should have been called');
-			assert.deepEqual(stateful.state, state);
-			assert.strictEqual(count, 2, 'listener called');
-		}
-	},
-	'toString()'(this: any) {
-		if (!hasToStringTag()) {
-			this.skip('Environment doesn\'t support Symbol.toStringTag');
-		}
-		const stateful: any = createStateful();
-		assert.strictEqual(stateful.toString(), '[object Stateful]');
+		stateful.on('state:changed', (event) => {
+			called = true;
+			assert.equal(event.target, stateful);
+			assert.equal(event.type, 'state:changed');
+			assert.deepEqual(event.state, state);
+		});
+		stateful.setState(state);
+		assert.isTrue(called);
 	}
 });
